b7da58b1d64494446af9b2c9e93c81d7ed8da274
Yiyi Wang <shd101wyy@sina.com>
2015-03-19 22:46:13 -0500

    huhh

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ var app = express();
 */
 app.use(express.static(__dirname + '/www'));
 
-var user = new User(
-    "ywang189",
-    "HsjWghW11",
-    "https://subversion.ews.illinois.edu/svn/sp15-cs242/ywang189"
-);
+var user = null;  // global variables
+var log_json = null;
+var list_json = null;
+
 /*
 user.queryLog(function(error){
     console.log(error);
@@ -29,20 +28,60 @@ app.get('/', function(req, res){
 // User Login
 app.post('/UserLogin', function(req, res){
     console.log("User Login");
-    var jsonString = '';
+    var post_data = '';
     if (req.method === "POST"){
         // begin to get data
         req.on("data", function(data){
-            jsonString += data;
+            post_data += data;
         });
         // finish loading data
         req.on('end', function(){
-            var jsonObject = querystring.parse(jsonString);
+            var jsonObject = querystring.parse(post_data);
             var user_name = jsonObject.user_name;
             var user_password = jsonObject.user_password;
             var user_svn_address = jsonObject.user_svn_address;
-            console.log(user_name + " " + user_password + " " + user_svn_address);
-            res.send(JSON.stringify(jsonObject));
+
+            // initialize user
+            user = new User(user_name, user_password, user_svn_address);
+            var query_log_result = user.queryLog();
+            var log_error = query_log_result.stderr.toString("utf8");
+            var log_string = query_log_result.stdout.toString("utf8");
+            if (log_error){
+                res.send("error"); // error
+                return;
+            }
+            var query_list_result = user.queryList();
+            var list_error = query_list_result.stderr.toString("utf8");
+            var list_string = query_list_result.stderr.toString("utf8");
+            if (list_error){
+                res.send("error");
+                return;
+            }
+
+            log_json = null;
+            list_json = null;
+            parseString(log_string, function(error, data){
+                if(error){
+                    res.send("error");
+                    return;
+                }
+                log_json = data;
+            });
+            parseString(list_string, function(error, data){
+                if (error){
+                    res.send("error");
+                    return;
+                }
+                list_json = data;
+            });
+            res.send("success");
+
+            /*
+            console.log("Result: ");
+            console.log(query_result.stdout.toString("utf8"));
+            console.log("Stderr: " + query_result.stderr.toString("utf8"));
+            console.log(query_result);
+            */
         });
     }
 });
--- svn_user.js
+++ svn_user.js
@@ -1,4 +1,5 @@
 var exec = require('child_process').exec;
+var spawnSync = require('child_process').spawnSync;
 function User(user_name, user_password, user_svn_address){
     this.user_name = user_name;
     this.user_password = user_password;
@@ -8,36 +9,22 @@ function User(user_name, user_password, user_svn_address){
 User.prototype.queryLog = function(error_function, success_function){
     // "svn log --verbose --xml https://subversion.ews.illinois.edu/svn/sp15-cs242/ywang189 --username ywang189 --password HsjWghW11"
     // set up function
-    var command = "svn log --verbose --xml " + this.user_svn_address + " --username " + this.user_name + "  --password " + this.user_password;
-    exec(command,
+    //var command = "svn log --verbose --xml " + this.user_svn_address + " --username " + this.user_name + "  --password " + this.user_password;
+    return spawnSync("svn",
+                     ["log", "--verbose", "--xml", this.user_svn_address, "--username", this.user_name, "--password", this.user_password],
         {
             timeout: 3000  // wait for 3 seconds, if no response, kill the process
-        },
-        function(error, stdout, stderr){
-            if (error){ // get error
-                error_function(error, stderr);
-            }
-            else{
-                success_function(stdout);
-            }
         });
 };
 
 User.prototype.queryList = function(error_function, success_function){
     // svn list --xml --recursive https://subversion.ews.illinois.edu/svn/fa13-cs242/{netid} > svn_list.xml
     // // setup function
-    var command = "svn list --xml --recursive " + this.user_svn_address + " --username " + this.user_name + "  --password " + this.user_password;
-    exec(command,
+    //var command = "svn list --xml --recursive " + this.user_svn_address + " --username " + this.user_name + "  --password " + this.user_password;
+    return spawnSync("svn",
+                    ["list", "--xml", "--recursive", this.user_svn_address, "--username", this.user_name, "--password", this.user_password],
         {
             timeout: 3000  // wait for 3 seconds, if no response, kill the process
-        },
-        function(error, stdout, stderr){
-            if (error){ // get error
-                error_function(error, stderr);
-            }
-            else{
-                success_function(stdout);
-            }
         });
 };
 
